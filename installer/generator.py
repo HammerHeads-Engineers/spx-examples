19bb4ed6516dfee3d4b3fd9d9cd26f268133d66c
Aleksander Stanik <aleksander.stanik@hammerheadesengineers.com>
2026-01-30 08:34:42 +0100

    fix: update SPX server and UI image versions to v1.0.0-rc.49

--- installer/generator.py
+++ installer/generator.py
@@ -17,10 +17,10 @@ from . import paths
 
 
 SPX_SERVER_SERVICE_NAME = "spx-server"
-SPX_SERVER_IMAGE = "simplephysx/spx-server:v1.0.0-rc.47"
+SPX_SERVER_IMAGE = "simplephysx/spx-server:v1.0.0-rc.49"
 # SPX_SERVER_IMAGE = "spx-server:trial"
 SPX_UI_SERVICE_NAME = "spx-ui"
-SPX_UI_IMAGE = "simplephysx/spx-ui:v1.0.0-rc.48"
+SPX_UI_IMAGE = "simplephysx/spx-ui:v1.0.0-rc.49"
 
 
 class DeploymentGenerator:
