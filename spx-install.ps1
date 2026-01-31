1ca35d06892b1116517de38f365dac05098f82e5
Aleksander Stanik <olek.stanik@gmail.com>
2026-01-31 20:40:58 +0100

    fix: correct syntax for pip install command in Python module checks

--- spx-install.ps1
+++ spx-install.ps1
@@ -71,7 +71,7 @@ function Check-PythonModules {
     $moduleNames = $missing | ForEach-Object { $_.Module }
     $packages = $missing | ForEach-Object { $_.Package }
     Write-Host "[spx-install] Missing Python modules: $($moduleNames -join ', '). Installing via pip..."
-    & $PythonBin -m pip install --user @packages
+    & $PythonBin -m pip install --user @($packages)
     if ($LASTEXITCODE -ne 0) {
         throw "[spx-install] pip install failed"
     }
