5190a5ed89795df5cd1a08a9ca8accc0e7991a9b
Aleksander Stanik <aleksander.stanik@hammerheadesengineers.com>
2026-02-10 22:49:48 +0100

    Fix Prevac pack integration wrappers for spx client

--- tests/packs/embedded_lab_pack/integration/test_modbus_prevac_m1600pdc_ps_sut_example.py
+++ tests/packs/embedded_lab_pack/integration/test_modbus_prevac_m1600pdc_ps_sut_example.py
@@ -5,18 +5,12 @@
 import tests.shared.integration.modbus_prevac_m1600pdc_ps_sut_example as shared_prevac
 
 
-MODEL_KEY = "embedded_lab_pack__prevac_m1600pdc_ps"
-INSTANCE_KEY = "spx_prevac_m1600pdc_ps"
-
-
 class TestModbusPrevacM1600PDCPSPackIntegration(
     shared_prevac.TestModbusPrevacM1600PDCPSExampleIntegration
 ):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls._client.update_model_key(shared_prevac.MODEL_KEY, MODEL_KEY)
-        cls._client.update_instance_key(shared_prevac.INSTANCE_KEY, INSTANCE_KEY)
 
     def setUp(self):
         if shared_prevac.ModbusTcpClient is None:  # pragma: no cover - dependency missing
--- tests/packs/embedded_lab_pack/integration/test_modbus_prevac_xr40b_ec_sut_example.py
+++ tests/packs/embedded_lab_pack/integration/test_modbus_prevac_xr40b_ec_sut_example.py
@@ -5,18 +5,12 @@
 import tests.shared.integration.modbus_prevac_xr40b_ec_sut_example as shared_prevac
 
 
-MODEL_KEY = "embedded_lab_pack__prevac_xr40b_ec"
-INSTANCE_KEY = "spx_prevac_xr40b_ec"
-
-
 class TestModbusPrevacXR40BECPackIntegration(
     shared_prevac.TestModbusPrevacXR40BECExampleIntegration
 ):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls._client.update_model_key(shared_prevac.MODEL_KEY, MODEL_KEY)
-        cls._client.update_instance_key(shared_prevac.INSTANCE_KEY, INSTANCE_KEY)
 
     def setUp(self):
         if shared_prevac.ModbusTcpClient is None:  # pragma: no cover - dependency missing
